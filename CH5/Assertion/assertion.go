d0a885148f43e6aff46fa75a747e7a9418283bf0
Zen <zhangyiming748@gmail.com>
2021-03-21 16:16:54 +0800

    断言

--- CH5/Assertion/assertion.go
+++ CH5/Assertion/assertion.go
@@ -1,1 +1,20 @@
 package Assertion
+
+
+
+func assertion(i interface{}) string {
+	switch i.(type) {
+	case int:
+		return "is int"
+	case string:
+		return "is string"
+	case bool:
+		return "is bool"
+	case float64:
+		return "is float64"
+	case float32:
+		return "is float32"
+	default:
+		return "is unknown"
+	}
+}
--- CH5/Assertion/assertion_test.go
+++ CH5/Assertion/assertion_test.go
@@ -1,1 +1,13 @@
 package Assertion
+
+import "testing"
+
+func TestAssertion(t *testing.T) {
+	var (
+		float float32 =1.2
+		double float64 =1.2
+	)
+
+	t.Log(assertion(float))
+	t.Log(assertion(double))
+}
