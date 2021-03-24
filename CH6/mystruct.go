4fac229885efad770597bfdc048457d4935e2831
Zen <zhangyiming748@gmail.com>
2021-03-24 18:17:09 +0800

    结构体方法

--- CH6/mystruct.go
+++ CH6/mystruct.go
@@ -16,3 +16,28 @@ func mystruct() {
 	c.y=4
 	log.Println(c.Color)
 }
+
+type Optioner interface {
+	Name() string
+	IsValid() string
+}
+type OptionCommon struct {
+	ShortName string "short option name"
+	LongName string "kong option name"
+}
+type IntOption struct {
+	OptionCommon
+	Value,min,max int
+}
+func(option IntOption)Name()string{
+	return name(option.ShortName,option.LongName)
+}
+func (option IntOption)IsValid()bool  {
+	return option.min<=option.Value&&option.Value<=option.max
+}
+func name(shortName,longName string) string{
+	if longName==""{
+		return shortName
+	}
+	return longName
+}
