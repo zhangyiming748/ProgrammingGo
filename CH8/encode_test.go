f37a01b66be26ae5198726bcb5d2251b5a49c21f
zen <zhangyiming748@gmail.com>
2021-05-09 16:28:10 +0800

    编码json

---
+++ CH8/encode_test.go
@@ -0,0 +1,40 @@
+package CH8
+
+import (
+	"encoding/json"
+	"fmt"
+	//"strings"
+	"testing"
+	//"time"
+)
+
+func TestMarshal(t *testing.T) {
+	//create a map to save
+	c:=make(map[string]interface{})
+	c["name"]="zen"
+	c["title"]="programmer"
+	c["contact"]=map[string]interface{}{
+		"home":"415.333.3333",
+		"cell":"415.555.5555",
+	}
+	data,err:=json.Marshal(c)
+	if err!=nil{
+		t.Log(err)
+	}
+	fmt.Println(string(data))
+}
+func TestMarshalIndent(t *testing.T) {
+	//create a map to save
+	c:=make(map[string]interface{})
+	c["name"]="zen"
+	c["title"]="programmer"
+	c["contact"]=map[string]interface{}{
+		"home":"415.333.3333",
+		"cell":"415.555.5555",
+	}
+	data,err:=json.MarshalIndent(c,"","    ")
+	if err!=nil{
+		t.Log(err)
+	}
+	fmt.Println(string(data))
+}
