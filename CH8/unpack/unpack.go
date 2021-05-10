6ae7feb9159a0cec3368451f5a934ab84f1efc84
zen <zhangyiming748@gmail.com>
2021-05-10 19:49:07 +0800

    修改后

--- CH8/unpack/unpack.go
+++ CH8/unpack/unpack.go
@@ -1,4 +1,5 @@
 
+
 package main
 
 import (
@@ -11,14 +12,13 @@ import (
     "log"
     "os"
     "path/filepath"
-    "runtime"
     "strings"
 )
 
 func main() {
     log.SetFlags(0)
     if len(os.Args) == 1 || os.Args[1] == "-h" || os.Args[1] == "--help" {
-        fmt.Printf("usage: %s archive.{zip,tar,tar.gz}\n",
+        fmt.Printf("usage: %s archive.{zip,tar,tar.gz,tar.bz2}\n",
             filepath.Base(os.Args[0]))
         os.Exit(1)
 
@@ -33,7 +33,8 @@ func main() {
 }
 
 func validSuffix(filename string) bool {
-    for _, suffix := range []string{".zip", ".tar", ".tar.gz"} {
+    for _, suffix := range []string{".zip", ".tar", ".tar.gz",
+        ".tar.bz2"} {
         if strings.HasSuffix(filename, suffix) {
             return true
         }
@@ -44,27 +45,25 @@ func validSuffix(filename string) bool {
 func unpackArchive(filename string) error {
     if strings.HasSuffix(filename, ".zip") {
         return unpackZip(filename)
-    }else if strings.HasSuffix(filename,".tar.bz2"){
-        return unpackTarBz2(filename)
     }
     return unpackTar(filename)
 }
 
-func unpackZip(filename string) error {
-    reader, err := zip.OpenReader(filename)
-    if err != nil {
+func unpackZip(filename string) (err error) {
+    var reader *zip.ReadCloser
+    if reader, err = zip.OpenReader(filename); err != nil {
         return err
     }
     defer reader.Close()
     for _, zipFile := range reader.Reader.File {
-        name := sanitizedName(zipFile.Name)
-        mode := zipFile.Mode()
-        if mode.IsDir() {
-            if err = os.MkdirAll(name, 0755); err != nil {
+        filename := sanitizedName(zipFile.Name)
+        if strings.HasSuffix(zipFile.Name, "/") ||
+            strings.HasSuffix(zipFile.Name, "\\") {
+            if err = os.MkdirAll(filename, 0755); err != nil {
                 return err
             }
         } else {
-            if err = unpackZippedFile(name, zipFile); err != nil {
+            if err = unpackZippedFile(filename, zipFile); err != nil {
                 return err
             }
         }
@@ -72,14 +71,14 @@ func unpackZip(filename string) error {
     return nil
 }
 
-func unpackZippedFile(filename string, zipFile *zip.File) error {
-    writer, err := os.Create(filename)
-    if err != nil {
+func unpackZippedFile(filename string, zipFile *zip.File) (err error) {
+    var writer *os.File
+    if writer, err = os.Create(filename); err != nil {
         return err
     }
     defer writer.Close()
-    reader, err := zipFile.Open()
-    if err != nil {
+    var reader io.ReadCloser
+    if reader, err = zipFile.Open(); err != nil {
         return err
     }
     defer reader.Close()
@@ -94,31 +93,35 @@ func unpackZippedFile(filename string, zipFile *zip.File) error {
     return nil
 }
 
-func unpackTar(filename string) error {
-    file, err := os.Open(filename)
-    if err != nil {
+func unpackTar(filename string) (err error) {
+    var file *os.File
+    if file, err = os.Open(filename); err != nil {
         return err
     }
     defer file.Close()
-    var fileReader io.ReadCloser = file
+    var fileReader io.Reader = file
+    var decompressor *gzip.Reader
     if strings.HasSuffix(filename, ".gz") {
-        if fileReader, err = gzip.NewReader(file); err != nil {
+        if decompressor, err = gzip.NewReader(file); err != nil {
             return err
         }
-        defer fileReader.Close()
-    }else if strings.HasSuffix(filename,"bz2"){
-        if fileReader = bzip2.NewReader(file);err!=nil{
-            defer fileReader.Close()
-        }
+        defer decompressor.Close()
+    } else if strings.HasSuffix(filename, ".bz2") {
+        fileReader = bzip2.NewReader(file)
     }
-    reader := tar.NewReader(fileReader)
+    var reader *tar.Reader
+    if decompressor != nil {
+        reader = tar.NewReader(decompressor)
+    } else {
+        reader = tar.NewReader(fileReader)
+    }
     return unpackTarFiles(reader)
 }
 
-func unpackTarFiles(reader *tar.Reader) error {
+func unpackTarFiles(reader *tar.Reader) (err error) {
+    var header *tar.Header
     for {
-        header, err := reader.Next()
-        if err != nil {
+        if header, err = reader.Next(); err != nil {
             if err == io.EOF {
                 return nil // OK
             }
@@ -131,8 +134,7 @@ func unpackTarFiles(reader *tar.Reader) error {
                 return err
             }
         case tar.TypeReg:
-            if err = unpackTarFile(filename, header.Name, reader);
-                err != nil {
+            if err = unpackTarFile(filename, header.Name, reader); err != nil {
                 return err
             }
         }
@@ -141,9 +143,9 @@ func unpackTarFiles(reader *tar.Reader) error {
 }
 
 func unpackTarFile(filename, tarFilename string,
-    reader *tar.Reader) error {
-    writer, err := os.Create(filename)
-    if err != nil {
+    reader *tar.Reader) (err error) {
+    var writer *os.File
+    if writer, err = os.Create(filename); err != nil {
         return err
     }
     defer writer.Close()
@@ -159,11 +161,10 @@ func unpackTarFile(filename, tarFilename string,
 }
 
 func sanitizedName(filename string) string {
-    if len(filename) > 1 && filename[1] == ':' &&
-        runtime.GOOS == "windows" {
+    if len(filename) > 1 && filename[1] == ':' {
         filename = filename[2:]
     }
-    filename = filepath.ToSlash(filename)
-    filename = strings.TrimLeft(filename, "/.")
-    return strings.Replace(filename, "../", "", -1)
+    filename = strings.TrimLeft(filename, "\\/.")
+    filename = strings.Replace(filename, "../", "", -1)
+    return strings.Replace(filename, "..\\", "", -1)
 }
