eddcafe0e3cdada470a16f42e36200bc7c1397a2
zen <zhangyiming748@gmail.com>
2021-05-12 21:37:48 +0800

    unsafe包

--- CH9/unsafety/unsafety_test.go
+++ CH9/unsafety/unsafety_test.go
@@ -1,4 +1,23 @@
 package unsafety
 
+import (
+	"fmt"
+	"testing"
+	"unsafe"
+)
 
+func TestGetSlice(t *testing.T) {
+	s := make([]int, 9, 20)
+	var Len = *(*int)(unsafe.Pointer(uintptr(unsafe.Pointer(&s)) + uintptr(8)))
+	fmt.Println(Len, len(s)) // 9 9
+	var Cap = *(*int)(unsafe.Pointer(uintptr(unsafe.Pointer(&s)) + uintptr(16)))
+	fmt.Println(Cap, cap(s)) // 20 20
+}
+func TestGetMap(t *testing.T) {
+	mp := make(map[string]int)
+	mp["qcrao"] = 100
+	mp["stefno"] = 18
 
+	count := **(**int)(unsafe.Pointer(&mp))
+	fmt.Println(count, len(mp)) // 2 2
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module ProgrammingGo
 
 go 1.15
+
+require github.com/beevik/etree v1.1.0
