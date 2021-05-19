261e870464a6821b5f82b4f314eea89108712ca8
zen <zhangyiming748@gmail.com>
2021-05-19 16:06:05 +0800

    编码json

--- CH8/encode_test.go
+++ CH8/encode_test.go
@@ -10,30 +10,30 @@ import (
 
 func TestMarshal(t *testing.T) {
 	//create a map to save
-	c:=make(map[string]interface{})
-	c["name"]="zen"
-	c["title"]="programmer"
-	c["contact"]=map[string]interface{}{
-		"home":"415.333.3333",
-		"cell":"415.555.5555",
+	c := make(map[string]interface{})
+	c["name"] = "zen"
+	c["title"] = "programmer"
+	c["contact"] = map[string]interface{}{
+		"home": "415.333.3333",
+		"cell": "415.555.5555",
 	}
-	data,err:=json.Marshal(c)
-	if err!=nil{
+	data, err := json.Marshal(c)
+	if err != nil {
 		t.Log(err)
 	}
 	fmt.Println(string(data))
 }
 func TestMarshalIndent(t *testing.T) {
 	//create a map to save
-	c:=make(map[string]interface{})
-	c["name"]="zen"
-	c["title"]="programmer"
-	c["contact"]=map[string]interface{}{
-		"home":"415.333.3333",
-		"cell":"415.555.5555",
+	c := make(map[string]interface{})
+	c["name"] = "zen"
+	c["title"] = "programmer"
+	c["contact"] = map[string]interface{}{
+		"home": "415.333.3333",
+		"cell": "415.555.5555",
 	}
-	data,err:=json.MarshalIndent(c,"","    ")
-	if err!=nil{
+	data, err := json.MarshalIndent(c, "", "    ")
+	if err != nil {
 		t.Log(err)
 	}
 	fmt.Println(string(data))
