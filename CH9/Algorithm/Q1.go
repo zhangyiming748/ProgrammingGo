afd36e796745e8feb96d211c3aeca4f0d29ff33f
zen <zhangyiming748@gmail.com>
2021-05-23 11:40:08 +0800

    学习自定义结构体多条件排序

--- CH9/Algorithm/Q1.go
+++ CH9/Algorithm/Q1.go
@@ -1,24 +1,23 @@
 package CH9
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
-/*
-标题：按身高和体重排队 时间限制：1秒 内存限制：262144K 语言：不限
-
-某学校举行运动会，学生们按编号1、2、3、....n进行标识，现需要按照身高由低到高排列，对身高相同的人，按体重由轻到重排列；对身高和体重都相同的人，维持原有的编号顺序关系。请输出排列后的学生编号。
-
-输入描述：两个序列，每个序列由n个正整数组成0<n<=100。第一个序列中的数值代表身高，第二个序列中的数值代表体重。
-输出描述：排列结果，每个数值都是原始序列中的学生编号，编号从1开始
-
-比如：
-输入：4 100 100 120 130 40 30 60 50
-输出：2 1 3 4
-*/
 type Student struct {
+	No     int
 	Height int
 	Weight int
 }
 
+func (s *Student) SetNO(i int) {
+	s.No = i
+	return
+}
+func (s Student) GetNO() int {
+	return s.No
+}
 func (s *Student) SetH(i int) {
 	s.Height = i
 	return
@@ -34,27 +33,27 @@ func (s Student) GetW() int {
 	return s.Weight
 }
 func Q1() {
-
-	list := []int{4, 100, 100, 120, 130, 40, 30, 60, 50}
-	m := make(map[int]Student)
-	for i := 0; i < list[0]; i++ {
-		h:=list[i+1]
-		w:=list[i+5]
-		m[i+1]=setHW(h,w)
+	input := []int{4, 100, 100, 120, 130, 40, 30, 60, 50}
+	student := make([]*Student, 0, input[0])
+	for i := 0; i < input[0]; i++ {
+		s := &Student{}
+		s.SetNO(i + 1)
+		s.SetH(input[i+1])
+		s.SetW(input[i+5])
+		student = append(student, s)
 	}
-
-	for key,value:=range m{
-		fmt.Println(key,value)
+	fmt.Println("before sort")
+	for _, v := range student {
+		fmt.Println(*v)
 	}
-}
-func setHW(h,w int)(s Student){
-	s.SetH(h)
-	s.SetW(w)
-	return s
-}
-func desc(m map[int]Student) {
-
-	for k,v:=range m{
-
+	sort.Slice(student, func(i, j int) bool {
+		if student[i].Height == student[j].Height {
+			return student[i].Weight < student[j].Weight
+		}
+		return student[i].Height < student[j].Height
+	})
+	fmt.Println("after sort")
+	for _, v := range student {
+		fmt.Println(*v)
 	}
 }
---
+++ CH9/Algorithm/Q1_test.go
@@ -0,0 +1,7 @@
+package CH9
+
+import "testing"
+
+func TestQ1(t *testing.T) {
+	Q1()
+}
