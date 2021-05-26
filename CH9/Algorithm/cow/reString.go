9130be9c8053a38da6f6228a82cfddff7bcd9008
zen <zhangyiming748@gmail.com>
2021-05-26 21:25:21 +0800

    反转字符串

--- CH9/Algorithm/cow/reString.go
+++ CH9/Algorithm/cow/reString.go
@@ -1,1 +1,12 @@
 package cow
+
+func ReString(s string) string {
+	data := []byte{}
+	newdata := []byte{}
+	data = []byte(s)
+	for len(data) != 0 {
+		newdata = append(newdata, data[len(data)-1])
+		data = data[:len(data)-1]
+	}
+	return string(newdata)
+}
