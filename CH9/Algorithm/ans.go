55392c3aa39fb2d7877e3695cc2d90db1bbd5da8
zen <zhangyiming748@gmail.com>
2021-05-26 21:23:57 +0800

    提交前

--- CH9/Algorithm/ans.go
+++ CH9/Algorithm/ans.go
@@ -1,41 +1,14 @@
 package main
 
-/**
- * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
- * 将给定数组排序
- * @param arr int整型一维数组 待排序的数组
- * @return int整型一维数组
- */
-func MySort( arr []int ) []int {
 
-	key := values[0]
-	p := 0
-	i,j := 0,len(arr)-1
-	for i  <= j {
 
-		for j >= p && values[j] >= key {
-			j--
-		}
-
-		if j >= p {
-			values[p] = values[j]
-			p = j
-		}
-		if values[i] <= key && i <= p {
-			i ++
-		}
-		if i < p{
-			values[p] = values[i]
-			p = i
-		}
-		values[p] = key
-		if p - left > 1{
-			quickSort(values, left, p-1)
-		}
-		if right - p > 1 {
-			quickSort(values, p+1, right)
-		}
+func ReString(str string) string {
+	data := []byte{}
+	newdata := []byte{}
+	data = []byte(str)
+	for len(data) != 0 {
+		newdata = append(newdata, data[len(data)-1])
+		data = data[:len(data)-1]
 	}
-
-	return arr
+	return string(newdata)
 }
