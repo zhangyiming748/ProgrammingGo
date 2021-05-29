5b66138f26975a757130eed4b5b3af06228f1fd2
zen <zhangyiming748@gmail.com>
2021-05-29 23:20:24 +0800

    完成

--- CH9/Algorithm/cow/FirstNotRepeatingChar.go
+++ CH9/Algorithm/cow/FirstNotRepeatingChar.go
@@ -1,11 +1,7 @@
 package cow
 
-import ()
-
 func FirstNotRepeatingChar(str string) int {
 	s := []byte(str)
-	//fmt.Println(s)
-	single := []byte{}
 	m := make(map[byte]int)
 	for _, v := range s {
 		if _, ok := m[v]; ok {
@@ -13,19 +9,10 @@ func FirstNotRepeatingChar(str string) int {
 		} else {
 			m[v] = 1
 		}
-		//fmt.Printf("循环中:%v\n",m)
 	}
-	for key, value := range m {
-		if value == 1 {
-			single = append(single, key)
-		}
-	}
-	for idx, val := range s {
-		for _, v := range single {
-			if val == v {
-				return idx
-
-			}
+	for i, value := range s {
+		if v, ok := m[value]; ok && v == 1 {
+			return i
 		}
 	}
 	return -1
