06d858a146a4c40f39a75461d530e7faecb3406b
zen <zhangyiming748@gmail.com>
2021-05-30 15:06:23 +0800

    即使使用sort包，提交后依然会报错

--- CH9/Algorithm/cow/mergeSlice.go
+++ CH9/Algorithm/cow/mergeSlice.go
@@ -2,16 +2,29 @@ package cow
 
 import (
 	"fmt"
+	"sort"
 )
 
 func merge(A []int, m int, B []int, n int) {
 	// write code here
 	ms := []int{}
+	if m == 0 && n != 0 {
+		sort.Ints(B)
+		A = B
+	}
+	if m != 0 && n == 0 {
+		sort.Ints(A)
+	}
+	if m == 0 && n == 0 {
+
+	}
 	for _, v := range A {
 		ms = append(ms, v)
 	}
 	for _, val := range B {
 		ms = append(ms, val)
 	}
+	sort.Ints(ms)
 	fmt.Println(ms)
+	A = ms
 }
