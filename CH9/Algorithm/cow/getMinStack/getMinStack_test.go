0276b963e61e599002491c56b051c1934f855c88
zen <zhangyiming748@gmail.com>
2021-06-08 19:11:49 +0800

    最小栈失败 重写

--- CH9/Algorithm/cow/getMinStack/getMinStack_test.go
+++ CH9/Algorithm/cow/getMinStack/getMinStack_test.go
@@ -24,68 +24,7 @@ import (
 数据保证没有不合法的操作
 
 */
-type Stack struct {
-	lens []int
-	top  int
-	min  int
-}
 
-func (s Stack) isEmpty() bool {
-	if len(s.lens) == 0 {
-		return true
-	}
-	return false
-}
-func (s *Stack) push(i int) {
-	s.lens = append(s.lens, i)
-
-	for _, v := range s.lens {
-		if v < s.min {
-			s.min = v
-		}
-	}
-	return
-}
-func (s *Stack) pop() {
-	if s.isEmpty() {
-		return
-	}
-	l.Info.Printf("pop前的stack%v\n", s.lens)
-
-	s.lens = s.lens[:len(s.lens)-1]
-	l.Info.Printf("pop后的stack%v\n", s.lens)
-	for _, v := range s.lens {
-		if v < s.min {
-			s.min = v
-		}
-	}
-	l.Info.Printf("pop后的stack%v\n", s.lens)
-
-	return
-
-}
-func (s *Stack) getMin() int {
-	return s.min
-}
-func (s Stack) getTop() int {
-	return s.lens[len(s.lens)-1]
-}
-func TestStack(t *testing.T) {
-	var s Stack
-	if s.isEmpty() {
-		t.Log("空栈")
-	}
-	s.push(1)
-	if s.isEmpty() {
-		t.Log("还是空栈")
-	}
-	ret := s.getTop()
-	t.Log(ret)
-	s.push(2)
-	t.Log(s.getTop())
-	s.pop()
-	t.Log(s.getTop())
-}
 func TestGetMinStack(t *testing.T) {
 	in := [][]int{{1, 3}, {1, 2}, {1, 1}, {3}, {2}, {3}}
 	ret := getMinStack(in)
@@ -95,24 +34,36 @@ func TestGetMinStack(t *testing.T) {
 /*
 1.push 2.pop 3.min
 */
+
 func getMinStack(op [][]int) []int {
-	//var s *Stack
-	s := new(Stack)
-	s.min = 1000000
-	var ans []int
+	stack := []int{}
+	min := 1000000
+	ans := []int{}
 	for i := 0; i < len(op); i++ {
-		if op[i][0] == 1 {
-			s.push(op[i][1])
-		}
 		if op[i][0] == 2 {
-			s.pop()
-		}
-		if op[i][0] == 3 {
-			if !s.isEmpty() {
-				ans = append(ans, s.getMin())
+			if len(stack) == 0 {
+				l.Debug.Println("没有可以弹出的")
+			} else {
+				stack = stack[:len(stack)-1]
+			}
+
+			for _, v := range stack {
+				if v < min {
+					min = v
+				}
 			}
 		}
+		if op[i][0] == 1 {
+			v := op[i][1]
+			if v < min {
+				min = v
+			}
+			stack = append(stack, v)
+		}
+
+		if op[i][0] == 3 {
+			ans = append(ans, min)
+		}
 	}
-	l.Info.Printf("op[0]`s type is %T", op[0])
 	return ans
 }
