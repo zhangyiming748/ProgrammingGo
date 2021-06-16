562a7fb8b05bc56a9d1dc55a6a0e3ca4cf8fc85f
zen <zhangyiming748@gmail.com>
2021-06-16 23:52:12 +0800

    思路 短除法取余

---
+++ CH9/Algorithm/cow/convertHex/convertHex_test.go
@@ -0,0 +1,21 @@
+package convertHex
+
+import "testing"
+
+/*
+描述
+给定一个十进制数M，以及需要转换的进制数N。将十进制数M转化为N进制数
+示例1
+输入：
+7,2
+复制
+返回值：
+"111"
+
+*/
+func solve(M int, N int) string {
+	// write code here
+}
+func TestSolve(t *testing.T) {
+
+}
