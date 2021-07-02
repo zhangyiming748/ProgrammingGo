917b7a563d4a4767d6bc2e35c381bd42a13d77e6
zhangyiming <zhangyiming748@gmail.com>
2021-07-02 21:09:00 +0800

    验证有效IP失败

--- CH9/Algorithm/cow/verification/verification_test.go
+++ CH9/Algorithm/cow/verification/verification_test.go
@@ -12,7 +12,7 @@ func solve(IP string) string {
 	if len(slice) > 1 {
 		res = typeV4(slice)
 	} else {
-		typeV6()
+		res = typeV6(IP)
 	}
 	return res
 }
@@ -24,13 +24,34 @@ func typeV4(s []string) string {
 	}
 	return "IPv4"
 }
-func typeV6() {
+func typeV6(s string) string {
+	slice := strings.Split(s, ":")
+	if len(slice) < 8 {
 
+	}
+	for _, v := range slice {
+		if v == "" {
+			return "Neither"
+		}
+	}
+	for _, v := range slice {
+		for _, val := range v {
+			if val >= 0 && val <= 9 || val >= 'A' && val <= 'F' || val >= 'a' && val <= 'f' {
+				continue
+			} else {
+				return "Neither"
+			}
+		}
+	}
+	return "IPv6"
 }
 func TestSolve(t *testing.T) {
 	ip := "172.16.234.1"
+	ipv6 := "2001:0db8:85a3:0:0:8A2E:0370:7334"
+	ret6 := solve(ipv6)
 	ret := solve(ip)
 	t.Logf(ret)
+	t.Logf("ipv6:%v", ret6)
 }
 
 //"172.16.254.1"
@@ -39,7 +60,9 @@ func TestSolve(t *testing.T) {
 IPv4 地址由十进制数和点来表示，每个地址包含4个十进制数，其范围为 0 - 255， 用(".")分割。比如，172.16.254.1；
 同时，IPv4 地址内的数不会以 0 开头。比如，地址 172.16.254.01 是不合法的。
 
-IPv6 地址由8组16进制的数字来表示，每组表示 16 比特。这些组数字通过 (":")分割。比如,  2001:0db8:85a3:0000:0000:8a2e:0370:7334 是一个有效的地址。而且，我们可以加入一些以 0 开头的数字，字母可以使用大写，也可以是小写。所以， 2001:db8:85a3:0:0:8A2E:0370:7334 也是一个有效的 IPv6 address地址 (即，忽略 0 开头，忽略大小写)。
+IPv6 地址由8组16进制的数字来表示，每组表示 16 比特。这些组数字通过 (":")分割。比如,  2001:0db8:85a3:0000:0000:8a2e:0370:7334 是一个有效的地址。
+而且，我们可以加入一些以 0 开头的数字，字母可以使用大写，也可以是小写。
+所以， 2001:db8:85a3:0:0:8A2E:0370:7334 也是一个有效的 IPv6 address地址 (即，忽略 0 开头，忽略大小写)。
 
 然而，我们不能因为某个组的值为 0，而使用一个空的组，以至于出现 (::) 的情况。 比如， 2001:0db8:85a3::8A2E:0370:7334 是无效的 IPv6 地址。
 同时，在 IPv6 地址中，多余的 0 也是不被允许的。比如， 02001:0db8:85a3:0000:0000:8a2e:0370:7334 是无效的。
