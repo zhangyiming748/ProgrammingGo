2ad709757e2ebfb72904c99ae286c68ffb464bde
zen <zhangyiming748@gmail.com>
2021-07-06 16:48:47 +0800

    failed

--- CH9/Algorithm/cow/myLRU/myLRU_test.go
+++ CH9/Algorithm/cow/myLRU/myLRU_test.go
@@ -1,70 +1,55 @@
 package myLRU
 
 import (
-	"ProgrammingGo/log"
+	"log"
 	"testing"
 )
 
-type list struct {
-	Length int         //cache容量
-	s      []int       //key的切片
-	m      map[int]int //key:value
+type myMaps struct {
+	limit int
+	keys  []int
+	value []int
 }
 
-func (l *list) push(key, value int) {
-	l.m[key] = value
-	log.Info.Printf("set %v:%v\n", key, value)
-	l.s = append(l.s, key)
-	log.Info.Printf("当前的cache列表%v\n", l.s)
-	if len(l.s) > l.Length {
-		delete(l.m, l.s[0])
-		l.s = l.s[1:]
-		log.Info.Printf("修剪过的cache列表%v\n", l.s)
-
-	}
-	return
-}
-func (l *list) pop(key int) int {
-	if v, ok := l.m[key]; ok {
-		value := v
-		log.Info.Printf("set %v:%v\n", key, value)
-		l.s = append(l.s, key)
-		log.Info.Printf("当前的cache列表%v\n", l.s)
-		if len(l.s) > l.Length {
-			delete(l.m, l.s[0])
-			l.s = l.s[1:]
-			log.Info.Printf("修剪过的cache列表%v\n", l.s)
+func (m *myMaps) get(n int) int {
+	for i := len(m.keys); i > len(m.keys)-m.limit; i-- {
+		if m.keys[i] == n {
+			m.keys = append(m.keys, i)
+			m.value = append(m.value, m.value[i])
+			return m.value[i]
 		}
-		return value
 	}
 	return -1
-
 }
+func (m *myMaps) set(k, v int) {
+	m.keys = append(m.keys, k)
+	m.value = append(m.value, v)
+	return
+}
+func TestMyLRU(t *testing.T) {
+	op := [][]int{{1, 1, 1}, {1, 2, 2}, {1, 3, 2}, {2, 1}, {1, 4, 4}, {2, 2}}
+	k := 3
+	ret := LRU(op, k)
+	t.Log(ret)
+}
+
 func LRU(operators [][]int, k int) []int {
-	op := operators
-	capp := k
-	l := new(list)
-	l.m = make(map[int]int)
-	l.Length = capp
-	l.s = make([]int, 0)
-	res := make([]int, 0)
-	for _, v := range op {
-		if len(v) == 3 {
-			key := v[1]
-			value := v[2]
-			l.push(key, value)
-		}
-		if len(v) == 2 {
-			key := v[1]
-			value := l.pop(key)
-			res = append(res, value)
+	var lru myMaps
+	lru.limit = k
+	res:=[]int{}
+	for i, v := range operators {
+		log.Printf("round %d\n", i+1)
+		if v[0] == 1 {
+			k:=v[1]
+			v:=v[2]
+			lru.set(k,v)
+		} else if v[0] == 2 {
+			k:=v[1]
+			res=append(res,lru.get(k))
+		} else {
+			log.Println("wrong op")
+			continue
 		}
 	}
 	return res
-}
-func TestLRU(t *testing.T) {
-	op := [][]int{{1, 1, 1}, {1, 2, 2}, {1, 3, 2}, {2, 1}, {1, 4, 4}, {2, 2}}
-	cap := 3
-	ret := LRU(op, cap)
-	t.Log(ret)
 }
