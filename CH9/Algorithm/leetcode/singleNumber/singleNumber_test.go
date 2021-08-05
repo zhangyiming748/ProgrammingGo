01dc1f33df862eb8e6c83ca35ab70a7ca082eb9c
zhangyiming <zhangyiming748@gmail.com>
2021-08-05 09:14:55 +0800

    format

--- CH9/Algorithm/leetcode/singleNumber/singleNumber_test.go
+++ CH9/Algorithm/leetcode/singleNumber/singleNumber_test.go
@@ -1,10 +1,11 @@
 package singleNumber
 
 import "testing"
+
 /*
 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
- */
-func singleNumber1(nums []int)int{
+*/
+func singleNumber1(nums []int) int {
 	m := make(map[int]int) //value=出现次数
 	for _, v := range nums {
 		if _, ok := m[v]; ok {
@@ -20,6 +21,7 @@ func singleNumber1(nums []int)int{
 	}
 	return 0
 }
+
 /*
 给你一个整数数组 nums ，除某个元素仅出现 一次 外，其余每个元素都恰出现 三次 。请你找出并返回那个只出现了一次的元素。
 */
